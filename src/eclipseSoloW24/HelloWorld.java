74c3d74deb0608e901769e5c665bbf8ff9f66a57
mevett <mevett@emich.edu>
2024-05-20 16:23:56 -0400

    Tweaking to see if github credentials are working.

--- src/eclipseSoloW24/HelloWorld.java
+++ src/eclipseSoloW24/HelloWorld.java
@@ -3,7 +3,7 @@ package eclipseSoloW24;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Alternate Howdy");
+		System.out.println("Alternatively Howdy");
 
 	}
 
